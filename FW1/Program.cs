2af06bcd7ecf1a8727349ec50f946cc4417cebeb
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2025-02-09 22:41:58 +0000

    Add configuration support for folder watching functionality

--- FW1/Program.cs
+++ FW1/Program.cs
@@ -1,8 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("FW1 Start");
 
+// Set the current directory to the directory where the executable is located
+//Directory.SetCurrentDirectory(AppContext.BaseDirectory);
 
-using var watcher = new FileSystemWatcher("./filedrop");
+string FolderToWatch = GetFolderToWatch();
+Console.WriteLine($"Watching folder: {FolderToWatch}");
+
+using var watcher = new FileSystemWatcher(FolderToWatch);
+
 
 watcher.NotifyFilter = NotifyFilters.Attributes
                      | NotifyFilters.CreationTime
@@ -12,16 +21,10 @@ watcher.NotifyFilter = NotifyFilters.Attributes
                      | NotifyFilters.LastWrite
                      | NotifyFilters.Security
                      | NotifyFilters.Size;
-
-
+                     
 watcher.Created += OnCreated;
-// watcher.Changed += OnChanged;
-// watcher.Deleted += OnDeleted;
-// watcher.Renamed += OnRenamed;
-// watcher.Error += OnError;
-
 watcher.Filter = "*.*";
-watcher.IncludeSubdirectories = true;
+watcher.IncludeSubdirectories = false;
 watcher.EnableRaisingEvents = true;
 
 Console.WriteLine("Press enter to exit.");
@@ -29,6 +32,30 @@ Console.ReadLine();
 
 static void OnCreated(object sender, FileSystemEventArgs e)
 {
-    string value = $"Created: {e.FullPath}";    
+    string value = $"Created: {e.FullPath}";
     Console.WriteLine(value);
+}
+
+static string GetFolderToWatch()
+{
+    var builder = Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration((hostingContext, configuration) =>
+        {
+            configuration.Sources.Clear();
+            var env = hostingContext.HostingEnvironment;
+            configuration.AddJsonFile("/workspaces/FileWatchPrint/FW1/appsettings.json", optional: true, reloadOnChange: true);
+            // FW1 / appsettings.json
+            // / workspaces / FileWatchPrint / FW1 / appsettings.json
+            configuration.AddEnvironmentVariables();
+        });
+
+    var host = builder.Build();
+
+    // Get the IConfiguration service from the built host
+    var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+    var folderPath = configuration["FileWatcher:FolderPath"];
+
+    return folderPath?.ToString() ?? "C:\\temp";
+
 }
